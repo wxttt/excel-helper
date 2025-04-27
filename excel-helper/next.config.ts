2ebd7753cbc1c094b028e5f42023a101157b0ad1
wxttt <wangxiaottt@gmail.com>
2025-04-27 08:30:09 +0800

    FIX：build 依赖错误

--- excel-helper/next.config.ts
+++ excel-helper/next.config.ts
@@ -3,9 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: [],
   },
 };
 
-export default nextConfig;
+export default nextConfig; 
